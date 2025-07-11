89e399d9efef69e135c6b0766b50afe9ee9679f0
1ngVG <giannini.v@outlook.com>
2025-07-11 19:14:40 +0200

    feat: setting pre commit plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,13 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
 rootProject.name = "CLI-Kotlin"
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.27"
+}
+
+//gitHooks {
+//    commitMsg {
+//        conventionalCommits { } // Only feat and fix
+//    }
+//    createHooks()
+//}
