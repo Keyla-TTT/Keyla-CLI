62d2a5e168f9cefb5ebd1232a7c9dbc1ea78af73
marco <marco.frattarola@studio.unibo.it>
2025-07-26 11:54:58 +0200

    feat: update project structure and dependencies for Kotlin multiplatform support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,99 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
-    }
+    kotlin("multiplatform") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
+}
 
-
-
-group = "org.example"
+group = "org.keyla"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
-    jvmToolchain(21)
+    jvm()
+    listOf(
+        linuxX64(), // Linux
+        mingwX64(), // Windows
+        macosArm64(), // Mac M1
+        macosX64(), // Mac Legacy
+    ).forEach { nativeTarget ->
+        nativeTarget.apply {
+            binaries {
+                executable {
+                    entryPoint = "org.keyla.main" // Point to the main function
+                }
+            }
+        }
+    }
+
+    sourceSets {
+        val ktorVersion = "2.3.7"
+        val kotterVersion = "1.2.1"
+        val coroutinesVersion = "1.7.3"
+        val serializationVersion = "1.7.3"
+
+        val commonMain by getting {
+            dependencies {
+                implementation("com.varabyte.kotter:kotter:$kotterVersion")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-cio:$ktorVersion")
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-mock:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
+            }
+        }
+
+        val linuxX64Main by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-curl:$ktorVersion")
+            }
+        }
+
+        val mingwX64Main by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-curl:$ktorVersion")
+            }
+        }
+
+        val macosArm64Main by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-curl:$ktorVersion")
+            }
+        }
+
+        val macosX64Main by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-curl:$ktorVersion")
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation("com.varabyte.kotterx:kotter-test-support:$kotterVersion")
+                implementation(kotlin("test"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
+            }
+        }
+    }
 }
 
+val appName = "keyla"
