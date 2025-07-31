c0a6b42e38db523b9047aaa44490afd108344b1c
marco <marco.frattarola@studio.unibo.it>
2025-07-31 17:20:05 +0200

    fix: fix release.yml

--- release.config.cjs
+++ release.config.cjs
@@ -10,8 +10,7 @@ module.exports = {
             {
                 assets: [
                     { path: 'dist/native-ubuntu-latest/keyla-linux', label: 'keyla-linux' },
-                    { path: 'dist/native-macos-latest/keyla-macos', label: 'keyla-macos' },
-                    { path: 'dist/native-macos-arm64/keyla-macos-arm64', label: 'keyla-macos-arm64' },
+                    { path: 'dist/native-macos-latest/keyla-macos-arm64', label: 'keyla-macos-arm64' },
                     { path: 'dist/native-windows-latest/keyla-windows.exe', label: 'keyla-windows.exe' }
                 ]
             }
